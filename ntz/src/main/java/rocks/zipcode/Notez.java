84a45cfb5d704c037056e74b1db850df9f97fb2b
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-04-05 15:12:05 -0400

    ok

--- ntz/src/main/java/rocks/zipcode/Notez.java
+++ ntz/src/main/java/rocks/zipcode/Notez.java
@@ -12,7 +12,7 @@ public final class Notez {
     }
     /**
      * Says hello to the world.
-     * 
+     *
      * @param args The arguments of the program.
      */
     public static void main(String argv[]) {
@@ -32,31 +32,41 @@ public final class Notez {
 
         /*
          * You will spend a lot of time right here.
-         * 
+         *
          * instead of loadDemoEntries, you will implement a series
          * of method calls that manipulate the Notez engine.
          * See the first one:
          */
-        ntzEngine.loadDemoEntries();
+        //ntzEngine.loadDemoEntries();
 
-        ntzEngine.saveDatabase();
+
 
         if (argv.length == 0) { // there are no commandline arguments
             //just print the contents of the filemap.
             ntzEngine.printResults();
         } else {
             if (argv[0].equals("-r")) {
+                //System.out.println("-r ram");
                 ntzEngine.addToCategory("General", argv);
-            } // this should give you an idea about how to TEST the Notez engine
+            } else if(argv[0].equals("-c")) {
+                ntzEngine.addToCategory(argv[1], argv);
+            }
+            // this should give you an idea about how to TEST the Notez engine
               // without having to spend lots of time messing with command line arguments.
         }
         /*
          * what other method calls do you need here to implement the other commands??
          */
+        ntzEngine.saveDatabase();
 
     }
 
-    private void addToCategory(String string, String[] argv) {
+    private void addToCategory(String category, String[] argv) {
+        if(filemap.containsKey(category)){
+            filemap.get(category).add(argv[argv.length-1]);
+        } else {
+            filemap.put(category, new NoteList(argv[argv.length-1]));
+        }
     }
 
     private void saveDatabase() {
